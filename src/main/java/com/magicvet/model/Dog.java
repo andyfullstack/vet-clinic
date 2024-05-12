30539663c9b0dcfe3796b2952ffb8a182c7ceef3
andyfullstack <andriikoliadenko@gmail.com>
2024-05-12 19:58:34 +0300

    REQ-04 Show client info (with pet) ver. 2

--- src/main/java/com/magicvet/model/Dog.java
+++ src/main/java/com/magicvet/model/Dog.java
@@ -1,7 +1,33 @@
 package main.java.com.magicvet.model;
 
+import java.util.Objects;
+
 public class Dog extends Pet {
     private String size;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                " age='" + super.getAge() + '\'' +
+                ", name='" + super.getName() + '\'' +
+                ", sex='" + super.getSex() + '\'' +
+                ", size='" + size + '\'' +
+                ", ownerName='" + super.getOwnerName() + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(size, dog.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), size);
+    }
 
     public void setSize(String size) {
         this.size = size;
