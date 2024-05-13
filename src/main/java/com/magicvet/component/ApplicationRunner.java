a5297c6f02106653bac5af022dac7200793912a4
andyfullstack <andriikoliadenko@gmail.com>
2024-05-13 18:56:39 +0300

    REQ-05 Remote pet type 'Other'

--- src/main/java/com/magicvet/component/ApplicationRunner.java
+++ src/main/java/com/magicvet/component/ApplicationRunner.java
@@ -22,10 +22,11 @@ public class ApplicationRunner {
                 if (addPet=='y' || addPet=='Y') {
                     System.out.println("Adding a new pet.");
                     Pet pet = petService.registerNewPet();
-                    client.setPet(pet);
-                    pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-                    System.out.println("Pet has been added.");
-
+                    if (pet != null){
+                        client.setPet(pet);
+                        pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
+                        System.out.println("Pet has been added.");
+                    }
                     System.out.println(client);
                 }
             }
