d310a841e63363baa0a9f4a60d184e46573d0326
andyfullstack <andriikoliadenko@gmail.com>
2024-05-19 19:40:59 +0300

    REQ-07: Use enum for dog size

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -2,10 +2,10 @@ package main.java.com.magicvet;
 
 import main.java.com.magicvet.comparator.DogAgeComparator;
 import main.java.com.magicvet.comparator.DogNameComparator;
-import main.java.com.magicvet.comparator.DogSizeComparator;
 import main.java.com.magicvet.model.Dog;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Sandbox {
     public static void main(String[] args) {
@@ -23,14 +23,14 @@ public class Sandbox {
         }*/
         //NEW   new Dog(String name, String age, String sex, String size)
         Dog[] dogs = { //NEW
-                new Dog("Rex", "3", "male", Dog.XL),
-                new Dog("Lessi", "5", "female", Dog.L),
-                new Dog("Sharic", "2", "male", Dog.M),
-                new Dog("Barbos", "10", "male", Dog.L),
-                new Dog("Penelopa", "3", "female", Dog.S),
-                new Dog("Monica", "2", "female", Dog.S)
+                new Dog("Rex", "3", "male", Dog.Size.XL),
+                new Dog("Lessi", "5", "female", Dog.Size.L),
+                new Dog("Sharic", "2", "male", Dog.Size.M),
+                new Dog("Barbos", "10", "male", Dog.Size.L),
+                new Dog("Penelopa", "3", "female", Dog.Size.S),
+                new Dog("Monica", "2", "female", Dog.Size.S)
         };
-        System.out.println("Sorting dogs by name:");
+        System.out.println("---Sorting dogs by name:");
         Arrays.sort(dogs, new DogNameComparator());
         for (Dog dog : dogs) {
             System.out.println(dog.getName() +
@@ -38,14 +38,21 @@ public class Sandbox {
                     ", sex = " + dog.getSex() +
                     ", size = " + dog.getSize() + ")");
         }
-        System.out.println("Sorting dogs by age:");
+        System.out.println("---Sorting dogs by age:");
         Arrays.sort(dogs, new DogAgeComparator());
         for (Dog dog : dogs) {
             System.out.println(dog.getAge() +
                     " (name = " + dog.getName() + ")");
         }
-        System.out.println("Sorting dogs by size:");
-        Arrays.sort(dogs, new DogSizeComparator());
+        System.out.println("---Sorting dogs by size:");
+        //Arrays.sort(dogs, new DogSizeComparator());
+        //Arrays.sort(dogs, new Comparator<Dog>()
+        Arrays.sort(dogs, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog o1, Dog o2) {
+                return o1.getSize().getValue() - o2.getSize().getValue();
+            }
+        });
         for (Dog dog : dogs) {
             System.out.println(dog.getSize() +
                     " (name = " + dog.getName() + ")");
--- src/main/java/com/magicvet/model/Dog.java
+++ src/main/java/com/magicvet/model/Dog.java
@@ -4,20 +4,14 @@ import java.util.Objects;
 
 public class Dog extends Pet {
 
-    public static final String XS = "XS";
-    public static final String S = "S";
-    public static final String M = "M";
-    public static final String L = "L";
-    public static final String XL = "XL";
-
-    private String size;
+    private Size size;
 
     public Dog() { }
 
-    public Dog(String size) {
+    public Dog(Size size) {
         this.size = size;
     }
-    public Dog(String name, String age, String sex, String size){ //NEW
+    public Dog(String name, String age, String sex, Size size){ //NEW
         super.setName(name);
         super.setAge(age);
         super.setSex(sex);
@@ -47,11 +41,32 @@ public class Dog extends Pet {
         return Objects.hash(super.hashCode(), size);
     }
 
-    public void setSize(String size) {
+    public void setSize(Size size) {
         this.size = size;
     }
 
-    public String getSize() {
+    public Size getSize() {
         return size;
     }
+
+    public enum Size {
+
+        XS(1),
+        S(2),
+        M(3),
+        L(4),
+        XL(5),
+        UNKNOWN(0);
+
+        private final int value;
+
+        Size(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }//enum Size
+
 }//Dog
